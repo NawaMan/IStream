550aacffb9fb1f7fc7cb87a2c4d18a8a05c532bf
Nawa Manusitthipol <nawa@nawaman.net>
2022-01-11 21:30:32 -0500

    Initial commit.

---
+++ src/test/java/net/nawaman/istream/ExtensibleStreamTest.java
@@ -0,0 +1,86 @@
+package net.nawaman.istream;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+
+class ExtensibleStreamTest {
+	
+	IStream<String> stream = IStream.of("A", "B", "C");
+	
+	<D> void compare(String expeced, Stream<D> actual) {
+		assertEquals(expeced, actual.collect(toList()).toString());
+	}
+	
+	@Test
+	void testCount() {
+		assertEquals(3, stream.count());
+	}
+	
+	@Test
+	void testIterator() {
+		var list = new ArrayList<String>();
+		for (var string : (Iterable<String>)stream::iterator) {
+			list.add(string);
+		}
+		assertEquals("[A, B, C]", list.toString());
+	}
+	
+	@Test
+	void testDistinct() {
+		compare("[A, B, C, A]", stream.concatWith("A"));
+		compare("[A, B, C]",    stream.concatWith("A").distinct());
+	}
+	
+	@Test
+	void testSorted() {
+		compare("[A, B, C, A]", stream.concatWith("A"));
+		compare("[A, A, B, C]", stream.concatWith("A").sorted());
+	}
+	
+	@Test
+	void testLimitLong() {
+		compare("[A, B]", stream.limit(2));
+	}
+	
+	@Test
+	void testSkipLong() {
+		compare("[B, C]", stream.skip(1));
+	}
+	
+	@Test
+	void testToArray() {
+		assertEquals("[A, B, C]", Arrays.toString(stream.toArray()));
+	}
+	
+	@Test
+	void testToArrayIntFunctionOfA() {
+		assertEquals("[A, B, C]", Arrays.toString(stream.toArray(String[]::new)));
+	}
+	
+	@Test
+	void testReduceDATABinaryOperatorOfDATA() {
+		assertEquals(">+A+B+C", stream.reduce(">", (a, b) -> a + "+" + b));
+	}
+	
+	@Test
+	void testReduceBinaryOperatorOfDATA() {
+		assertEquals("Optional[A+B+C]", stream.reduce((a, b) -> a + "+" + b).toString());
+	}
+	
+	@Test
+	void testFindFirst() {
+		assertEquals("A", stream.findFirst().get());
+	}
+	
+	@Test
+	void testFindAny() {
+		assertEquals("A", stream.findAny().get());
+	}
+	
+}
---
+++ src/test/java/net/nawaman/istream/IStreamTest.java
@@ -0,0 +1,49 @@
+package net.nawaman.istream;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+
+class IStreamTest {
+	
+	<D> void compare(String expeced, IStream<D> actual) {
+		assertEquals(expeced, actual.toList().toString());
+	}
+	
+	@Test
+	void testEmpty() {
+		compare("[]", IStream.empty());
+	}
+	
+	@Test
+	void testFromArray() {
+		compare("[A, B, C]", IStream.of      ("A", "B", "C"));
+		compare("[A, B, C]", IStream.forArray("A", "B", "C"));
+	}
+	
+	@Test
+	void testFromCollection() {
+		compare("[A, B, C]", IStream.from         (asList("A", "B", "C")));
+		compare("[A, B, C]", IStream.forCollection(asList("A", "B", "C")));
+	}
+	
+	@Test
+	void testConcat() {
+		compare("[A, B, C, D, E, F]", IStream.of("A", "B", "C").concatWith(Stream.of    ("D", "E", "F")));
+		compare("[A, B, C, D, E, F]", IStream.of("A", "B", "C").concatWith(Arrays.asList("D", "E", "F")));
+		compare("[A, B, C, D, E, F]", IStream.of("A", "B", "C").concatWith(              "D", "E", "F" ));
+	}
+	
+	@Test
+	void testConcatNull() {
+		compare("[A, B, C]", IStream.of("A", "B", "C").concatWith((Stream<String>)null));
+		compare("[A, B, C]", IStream.of("A", "B", "C").concatWith((List<String>)  null));
+		compare("[A, B, C]", IStream.of("A", "B", "C").concatWith((String[])      null));
+	}
+	
+}
